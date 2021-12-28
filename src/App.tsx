62f830b9c66c4489e3a076c1b3ad3f9038eb5aa3
german nunez <german90nunez@gmail.com>
2021-12-28 15:33:09 -0600

    enum and type

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import axios from 'axios';
+import {useState} from 'react';
+
+// Enum for difficulty
+enum Difficulty {
+  EASY = "easy",
+  MEDIUM="medium",
+  Hard="hard"
+}
+
+// Types
+type QuestionState = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+  answers: string[]
+}
+
+// Total Questions
+const TOTAL_QUESTIONS = 10
 
 const App = () => {
+  const [loader, setLoader] = useState(false)
+  const [data, setData] = useState({})
+
   return (
     <div className="quiz-container">
+      <h1>Quiz App</h1>
+      <div className="loader">Loading...</div>
+      <button className="start">Start</button>
+      <button className="next">Next Question</button>
     </div>
   );
 }
